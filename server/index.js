bee458723ad64196ba5ca1770fafd58baa655b3a
Thomas Houtekier <Thomas.Houtekier@realdolmen.com>
2020-02-05 12:19:31 +0100

    update skaffold config for local usage

--- server/index.js
+++ server/index.js
@@ -42,7 +42,8 @@ app.get('/', (req, res) => {
 app.get('/values/all', async (req, res) => {
   const values = await pgClient.query('SELECT * from values');
 
-  res.send(values.rows);
+  //res.send(values.rows);
+  res.send(["1","2","3"]);
 });
 
 app.get('/values/current', async (req, res) => {
